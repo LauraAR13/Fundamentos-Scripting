a9aa80d4257230e5784038aee6995e619d9851e5
Antonella Ruiz <53185381+LauraAR13@users.noreply.github.com>
2019-07-29 21:11:08 -0500

    Add files via upload

---
+++ Tarea 3.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion_tarea_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de los datos
+            Console.WriteLine("Ingrese el monto de su salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < 1656232)
+            {
+                Console.WriteLine("Usted pertenece a la tarifa A: ");
+                Console.WriteLine("Su cuota moderadora para citas generales es de $3200");
+            }
+
+            else if  (1656232 <= salario && salario < 3312464)
+            {
+                Console.WriteLine("Usted pertenece a la tarifa B: ");
+                Console.WriteLine("Su cuota moderadora para citas generales es de $12725");
+            }
+
+            else
+            {
+                Console.WriteLine("Usted pertenece a la tarifa C: ");
+                Console.WriteLine("Su cuota moderadora para citas generales es de $33500");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tarea3.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solucion_tarea_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Por favor ingrese el valor de su salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (  salario < 1656232)
+            {
+                Console.WriteLine("Usted pertenece a la tarifa A");
+            }
+
+            else if (1656232<= salario && salario < 3312464)
+            {
+                Console.WriteLine("Usted pertenece a a la tarifa B");
+            }
+
+            else
+            {
+                Console.WriteLine("Usted pertenece a la tarifa C");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
