0525e47126bad9b56c4d01fe3b0027425e4ac860
Antonella Ruiz <53185381+LauraAR13@users.noreply.github.com>
2019-08-13 20:19:53 -0500

    Add files via upload

---
+++ BlackJack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+
+            Console.WriteLine("Bienvenido a BlackJack");
+
+            int carta1 = aleatorio.Next(1, 10);
+            Console.WriteLine("Su primera carta es: " + carta1);
+            int carta2 = aleatorio.Next(1, 10);
+            Console.WriteLine("Su segunda carta es: " + carta2);
+
+            int suma = carta1 + carta2;
+            Console.WriteLine("Lleva un total de: " + suma);
+            int nuevaCarta = 0;
+
+            String continuar = "s";
+            while (continuar == "s" && suma <= 21)
+            {
+
+                Console.WriteLine("¿Desea pedir otra carta? (s/n)");
+                continuar = Console.ReadLine();
+                Console.WriteLine("");
+                if (continuar == "s")
+                {
+                    nuevaCarta = aleatorio.Next(1, 10);
+                    Console.WriteLine("Su nueva carta es: " + nuevaCarta);
+                    suma = suma + nuevaCarta;
+                    Console.WriteLine("Su nuevo total es de: " + suma);
+
+                }
+                if (suma > 21)
+                {
+                    Console.WriteLine("Eliminado");
+                    continuar = "n";
+
+                }
+
+                Console.WriteLine("Su total fue de: " + suma);
+                Console.WriteLine("Gracias por jugar");
+
+            }
+        }
+    }
+}
