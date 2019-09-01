00a0fdf2ed2413fb28a16203359ea59de380fb5b
Antonella Ruiz <53185381+LauraAR13@users.noreply.github.com>
2019-09-01 17:47:15 -0500

    Add files via upload

---
+++ 02.Fundamentos de programacion ejercicio 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Fundamentos_de_programación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos 
+            Console.WriteLine("Ingrese el valor de la coordenada x1: ");
+            double  x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la coordenada y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la coordenada x2: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de la coordenada y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            //Operaciones 
+            double m = (y2 - y1) / (x2 - x1); // Cálculo de la pendiente 
+            double b = ((y1) - (m)*(x1)); //Cálculo de el intercepto
+            double distancia = Math.Sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+
+            //Salida de datos 
+            Console.WriteLine("La pendiente es: " + m);
+            Console.WriteLine("El intercepto es: " + b);
+            Console.WriteLine("La distancia es: " + distancia);
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Conversion de binarios a decimal ejercicio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_binarios_a_decimales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b5: ");
+            int b5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b4: ");
+            int b4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b3: ");
+            int b3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b2: ");
+            int b2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b1: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 1 o 0 para el valor de  b0: ");
+            int b0 = int.Parse(Console.ReadLine());
+
+            //Operaciones 
+            double valordecimal = Math.Pow(b5 * 2, 5) + Math.Pow(b4 * 2, 4) + Math.Pow(b3 * 2, 3) + Math.Pow(b2 * 2, 2) + Math.Pow(b1 * 2, 1) + Math.Pow(b0 * 2, 0);
+            Console.WriteLine("El valor equivalente en decimal es: " + valordecimal);
+        }
+    }
+}
