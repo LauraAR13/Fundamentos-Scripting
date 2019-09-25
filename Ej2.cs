8a10dcc31d2b49a064002a63dad987e89d32fcc8
Antonella Ruiz <53185381+LauraAR13@users.noreply.github.com>
2019-09-25 17:19:45 -0500

    Add files via upload

---
+++ Ej2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    
+    Renderer sphRenderer;
+   float   t =0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        sphRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        Color r = new Color(1f, 0, 0);
+        Color b = new Color(0, 0, 1f);
+        Color p = new Color(0.87f, 0.35f, 1f);
+        if (t <= 3)
+        {
+            sphRenderer.material.color = r;
+        }
+        else if (t >= 3 && t < 6)
+        {
+            sphRenderer.material.color = b;
+        }
+        else if (t >= 6)
+        {
+            sphRenderer.material.color = p;
+        }
+
+    }
+}
---
+++ Ej3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Light luz; 
+    void Start()
+    {
+       luz = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color b = new Color(0, 0, 1f);
+        Color r = new Color(1f, 0, 0);
+        Color w = new Color(1f, 1f, 1f);
+        if (luz.intensity <= 0.25 && luz.intensity <=0.5 )
+        {
+            luz.color = b;
+        }
+        else if (luz.intensity > 0.5 && luz.intensity <= 0.75)
+        {
+            luz.color = r; 
+        }
+        else if (luz.intensity > 0.75)
+        {
+            luz.color = w;
+        }
+    }
+}
