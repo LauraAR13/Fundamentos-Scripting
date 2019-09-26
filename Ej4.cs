07d69d49c565178cb6c7043841f3cfdcfeba6ea4
Antonella Ruiz <53185381+LauraAR13@users.noreply.github.com>
2019-09-25 22:21:59 -0500

    Add files via upload

---
+++ Ej4.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej4 : MonoBehaviour
+{
+    Light luz;
+    Transform sptransform;
+    
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        luz = GetComponent<Light>();
+        sptransform = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        luz.intensity = sptransform.localScale.magnitude * 0.5f;
+    }
+}
